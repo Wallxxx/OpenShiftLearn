40f12ddc4a3abbc05d894c349df07a1113f9e36e
Wallxxx <tvaibabyshka@gmail.com>
2024-01-23 21:47:52 +0300

    Build pipeline code

--- Jenkins/build/pipeline.groovy
+++ Jenkins/build/pipeline.groovy
@@ -1,8 +1,30 @@
 pipeline {
     agent any
     stages {
+        stage('Clone git repo') {
+            steps {
+                script {
+                    checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/Wallxxx/OpenShiftLearn.git']]])
+                }
+            }
+        }
+
         stage('Build') {
-            print("Hello")
+            steps {
+                script {
+                    sh 'mvn clean package'
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo 'Success'
+            archiveArtifacts 'target/OpenShiftLearn-1.0.jar'
+        }
+        failure {
+            echo 'Failure'
         }
     }
 }
